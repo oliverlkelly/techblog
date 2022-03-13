563a4c022763e5f99f82dc9b80b5e62e1b25ba7f
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-03-14 01:53:56 +1100

    adding routes files and adjusting routes top level index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next){
-    res.render('index', {layout: 'main', title: 'Testing', title2: 'Testing'});
+router.get('/', function(req, res){
+    res.render('index', {layout: 'main', title: 'Testing'});
 });
-router.get('/login', function(req, res, next){
-    res.render('login', {layout: 'main', title: 'Testing', title2: 'Testing'});
+router.get('/login', function(req, res){
+    res.render('login', {layout: 'main', title: 'Testing'});
 });
-router.get('/signup', function(req, res, next){
-    res.render('signup', {layout: 'main', title: 'Testing', title2: 'Testing'});
+router.get('/signup', function(req, res){
+    res.render('signup', {layout: 'main', title: 'Testing'});
 });
 
 module.exports = router;

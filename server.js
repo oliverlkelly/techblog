45734ccd309af17b7390834b5dbdf8cb68b0f8c0
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-03-14 01:40:04 +1100

    commenting out herouku lines, adding local deploy line back in

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const hbs = handlebars.create()
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(routes);
-app.listen(process.env.PORT || 3000, '0.0.0.0', function(){
-    console.log("Server Start");
-});
+app.listen(3001);
+// app.listen(process.env.PORT || 3000, '0.0.0.0', function(){
+//     console.log("Server Start");
+// });

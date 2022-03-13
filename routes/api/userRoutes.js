6524b5aff1e84814b696afc2533eb21eb3e4a3e4
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-03-14 02:27:11 +1100

    working on API routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,3 +2,37 @@ const router = require('express').Router();
 const {User, Article, Comment} = require('../../models');
 const session = require('express-session');
 
+router.get('/', (res, req) => {
+    User.findAll({
+        attributes: {exclude: ['password']}
+    })
+    .then((userData) => res.JSON(userData))
+    .catch((err) =>{
+        console.log(err);
+        res.status(500).JSON(err);
+    });
+});
+
+router.get('/:id', (res, req) => {
+    User.findOne({
+        attributes: {exclude: ['password']},
+        where:{
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Article,
+                attributes: ['id', 'title', 'content', 'created']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'content', 'articleId', 'userId']
+            }
+        ]
+    })
+    .then((userData) => res.JSON(userData))
+    .catch((err) =>{
+        console.log(err);
+        res.status(500).JSON(err);
+    });
+});

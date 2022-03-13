4b19fbb1a9581acd000183f26a9b4aad30914b8e
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-03-14 00:59:45 +1100

    adding comment model

---
+++ models/Comment.js
@@ -0,0 +1,42 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        content:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userId:{
+            type: DataTypes.INTEGER,
+            references:{
+                model: 'user',
+                key: 'id'
+            }
+        },
+        articleId:{
+            type: DataTypes.INTEGER,
+            references:{
+                model: 'article',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment;

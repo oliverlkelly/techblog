7a75e2f82c60a5ca663bcd1eee2067bffc35ffd7
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-03-13 23:55:49 +1100

    adding extra routes

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,11 @@ var router = express.Router();
 router.get('/', function(req, res, next){
     res.render('index', {layout: 'main', title: 'Testing', title2: 'Testing'});
 });
+router.get('/login', function(req, res, next){
+    res.render('login', {layout: 'main', title: 'Testing', title2: 'Testing'});
+});
+router.get('/signup', function(req, res, next){
+    res.render('signup', {layout: 'main', title: 'Testing', title2: 'Testing'});
+});
 
 module.exports = router;

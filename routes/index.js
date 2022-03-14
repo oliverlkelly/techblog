9e5f19e09fc5e9bea635fce3d306c1f6e7e99888
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-03-15 01:40:15 +1100

    working on styling and form login for login, adding pages and styling to handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,23 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
 
 router.get('/', function(req, res){
     res.render('index', {layout: 'main', title: 'Testing'});
 });
-router.get('/login', function(req, res){
-    res.render('login', {layout: 'main', title: 'Testing'});
+
+router.get('/dashboard', function(req, res){
+    res.render('dashboard', {layout: 'main', title: 'Testing'});
 });
+
+router.get('/login', function(req, res){
+    res.render('login', {layout: 'loginout', title: 'Testing'});
+});
+router.post('/login', )
+
 router.get('/signup', function(req, res){
-    res.render('signup', {layout: 'main', title: 'Testing'});
+    res.render('signup', {layout: 'loginout', title: 'Testing'});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,34 @@
-var express = require('express');
-var path = require('path');
-var handlebars = require('express-handlebars');
+const express = require('express');
+const path = require('path');
+const handlebars = require('express-handlebars');
+const hbs = handlebars.create();
+const routes = require('./routes');
+const session = require('express-session');
 
-var routes = require('./routes');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sess));
 
-var app = express();
-const hbs = handlebars.create()
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
-app.listen(3001);
-// app.listen(process.env.PORT || 3000, '0.0.0.0', function(){
-//     console.log("Server Start");
-// });
+
+
+app.listen(PORT, () =>{
+    console.log("Listening on: http://localhost:"+PORT);
+});
